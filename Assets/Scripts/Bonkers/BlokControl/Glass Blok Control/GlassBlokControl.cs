bda1b7ac4998611ddb3f1bcba7ce66d1cd05873e
Joshua Steward <steward.joshua@rocketmail.com>
2023-06-01 21:51:10 +0900

    new Temhotaokeah sprites imported and set up on prefabs

--- Assets/Scripts/Bonkers/BlokControl/Glass Blok Control/GlassBlokControl.cs
+++ Assets/Scripts/Bonkers/BlokControl/Glass Blok Control/GlassBlokControl.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Bonkers.Combat;
-using Bonkers.Score;
+﻿using UnityEngine;
 
 namespace Bonkers.BlokControl
 {
